a113462084b1e248aeb673c56188d2b4212dcb66
quyenanh <anhnq@tmh-techlab.vn>
2019-02-13 20:01:17 +0700

    popup

---
+++ PLTi2019/Utils/object.swift
@@ -0,0 +1,22 @@
+//
+//  books.swift
+//  PLTi2019
+//
+//  Created by Quyen Anh on 2/13/19.
+//  Copyright © 2019 Quyen Anh. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    @objc dynamic var name:             String = ""
+    @objc dynamic var author:           String = ""
+    @objc dynamic var description:      String = ""
+
+}
+
+class Duration {
+    @objc dynamic var day:              Int = 0
+    @objc dynamic var month:            Int = 0
+    @objc dynamic var year:             Int = 0
+}
