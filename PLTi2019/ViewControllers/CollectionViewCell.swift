f8a6fd40417999caff2fdd0759cf616fe7b900f3
quyenanh <anhnq@tmh-techlab.vn>
2019-02-25 10:52:59 +0700

    continue

---
+++ PLTi2019/ViewControllers/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  PLTi2019
+//
+//  Created by Quyen Anh on 2/21/19.
+//  Copyright © 2019 Quyen Anh. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    
+}
