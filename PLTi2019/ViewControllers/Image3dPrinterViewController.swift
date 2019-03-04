aa55970a16c673b774531b21c0bb49c9d5d8f069
Dinh Nhinh <dinhnhinh@Dinhs-MacBook.local>
2019-03-04 14:22:59 +0700

    show image Print3D

--- PLTi2019/ViewControllers/Image3dPrinterViewController.swift
+++ PLTi2019/ViewControllers/Image3dPrinterViewController.swift
@@ -7,6 +7,14 @@
 //
 
 import UIKit
+import ImageViewer
+
+extension UIImageView: DisplaceableView {}
+
+struct DataItem {
+    let imageView: UIImageView
+    let galleryItem: GalleryItem
+}
 
 class Image3dPrinterViewController: UIViewController {
     let imageNames = ["img1","img2","img3","img4","img5", "img6"]
@@ -16,12 +24,20 @@ class Image3dPrinterViewController: UIViewController {
     @IBOutlet weak var largeImg: UIImageView!
     @IBOutlet var largeImgView: UIView!
     
+    var items: [DataItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         //        gestureSwipe()
-        // Do any additional setup after loading the view.
+        
+        for (_, imgString) in imageNames.enumerated() {
+            
+            var galleryItem: GalleryItem!
+            let image = UIImage(named: imgString) // ?? UIImage(named: "0")!
+            galleryItem = GalleryItem.image { $0(image) }
+            items.append(DataItem(imageView: largeImg, galleryItem: galleryItem))
+        }
     }
     
     func setupView() {
@@ -50,25 +66,6 @@ class Image3dPrinterViewController: UIViewController {
         // call api to get collection
     }
     
-    func gestureSwipe() {
-        let left = UISwipeGestureRecognizer(target : self, action : #selector(self.leftSwipe))
-        left.direction = .left
-        self.view.addGestureRecognizer(left)
-        
-        let right = UISwipeGestureRecognizer(target : self, action : #selector(self.rightSwipe))
-        right.direction = .right
-        self.view.addGestureRecognizer(right)
-        
-    }
-    
-    @objc func rightSwipe() {
-        print("dep trai")
-    }
-    
-    @objc func leftSwipe() {
-        print("dep trai")
-    }
-    
     @IBAction func backHome(_ sender: Any) {
         let stboard = UIStoryboard.init(name: "Main", bundle: nil)
         let loginVC = stboard.instantiateViewController(withIdentifier: "homeVC")
@@ -82,6 +79,7 @@ class Image3dPrinterViewController: UIViewController {
     
 }
 
+//MARK: Extention collection view
 extension Image3dPrinterViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.imageNames.count
@@ -95,24 +93,126 @@ extension Image3dPrinterViewController: UICollectionViewDataSource, UICollection
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        // show pop up
-        showLargeImg(index: indexPath.row)
+        showImage(index: indexPath.row)
         
     }
     
-    func showLargeImg(index: Int) {
-        self.view.addSubview(largeImg)
-        largeImgView.center = self.view.center
-        largeImgView.transform = CGAffineTransform.init(scaleX: 0.5, y: 0.5)
-        largeImgView.alpha = 0
+    func showImage(index: Int) {
+        let displacedViewIndex: Int = index
+        let frame = CGRect(x: 0, y: 0, width: 200, height: 24)
+//        let headerView = CounterView(frame: frame, currentIndex: displacedViewIndex, count: items.count)
+        let footerView = CounterView(frame: frame, currentIndex: displacedViewIndex, count: items.count)
         
-        largeImg.image = UIImage(named: imageNames[index])
+        let galleryViewController = GalleryViewController(startIndex: displacedViewIndex, itemsDataSource: self, itemsDelegate: self, displacedViewsDataSource: self, configuration: galleryConfiguration())
+//        galleryViewController.headerView = headerView
+        galleryViewController.footerView = footerView
         
-        UIView.animate(withDuration: 0.4) {
-            self.largeImgView.alpha = 1
-            self.largeImgView.transform = CGAffineTransform.identity
+        galleryViewController.launchedCompletion = { print("LAUNCHED") }
+        galleryViewController.closedCompletion = { print("CLOSED") }
+        galleryViewController.swipedToDismissCompletion = { print("SWIPE-DISMISSED") }
+        
+        galleryViewController.landedPageAtIndexCompletion = { index in
+            
+            print("LANDED AT INDEX: \(index)")
+            
+//            headerView.count = self.items.count
+//            headerView.currentIndex = index
+            footerView.count = self.items.count
+            footerView.currentIndex = index
         }
         
+        self.presentImageGallery(galleryViewController)
+    }
+    
+    func galleryConfiguration() -> GalleryConfiguration {
+        
+        return [
+            
+            GalleryConfigurationItem.closeButtonMode(.builtIn),
+            
+            GalleryConfigurationItem.pagingMode(.standard),
+            GalleryConfigurationItem.presentationStyle(.displacement),
+            GalleryConfigurationItem.hideDecorationViewsOnLaunch(false),
+            
+            GalleryConfigurationItem.swipeToDismissMode(.vertical),
+            GalleryConfigurationItem.toggleDecorationViewsBySingleTap(false),
+            GalleryConfigurationItem.activityViewByLongPress(false),
+            
+            GalleryConfigurationItem.overlayColor(UIColor(white: 0.035, alpha: 1)),
+            GalleryConfigurationItem.overlayColorOpacity(1),
+            GalleryConfigurationItem.overlayBlurOpacity(1),
+            GalleryConfigurationItem.overlayBlurStyle(UIBlurEffect.Style.light),
+            
+            GalleryConfigurationItem.videoControlsColor(.white),
+            
+            GalleryConfigurationItem.maximumZoomScale(8),
+            GalleryConfigurationItem.swipeToDismissThresholdVelocity(500),
+            
+            GalleryConfigurationItem.doubleTapToZoomDuration(0.15),
+            
+            GalleryConfigurationItem.blurPresentDuration(0.5),
+            GalleryConfigurationItem.blurPresentDelay(0),
+            GalleryConfigurationItem.colorPresentDuration(0.25),
+            GalleryConfigurationItem.colorPresentDelay(0),
+            
+            GalleryConfigurationItem.blurDismissDuration(0.1),
+            GalleryConfigurationItem.blurDismissDelay(0.4),
+            GalleryConfigurationItem.colorDismissDuration(0.45),
+            GalleryConfigurationItem.colorDismissDelay(0),
+            
+            GalleryConfigurationItem.itemFadeDuration(0.3),
+            GalleryConfigurationItem.decorationViewsFadeDuration(0.15),
+            GalleryConfigurationItem.rotationDuration(0.15),
+            
+            GalleryConfigurationItem.displacementDuration(0.55),
+            GalleryConfigurationItem.reverseDisplacementDuration(0.25),
+            GalleryConfigurationItem.displacementTransitionStyle(.springBounce(0.7)),
+            GalleryConfigurationItem.displacementTimingCurve(.linear),
+            
+            GalleryConfigurationItem.statusBarHidden(true),
+            GalleryConfigurationItem.displacementKeepOriginalInPlace(false),
+            GalleryConfigurationItem.displacementInsetMargin(50)
+        ]
     }
 }
 
+extension Image3dPrinterViewController: GalleryDisplacedViewsDataSource {
+    
+    func provideDisplacementItem(atIndex index: Int) -> DisplaceableView? {
+//        print("items[index].imageView \(items[index].imageView)")
+        return index < items.count ? items[index].imageView : nil
+    }
+}
+
+extension Image3dPrinterViewController: GalleryItemsDataSource {
+    
+    func itemCount() -> Int {
+        
+        return items.count
+    }
+    
+    func provideGalleryItem(_ index: Int) -> GalleryItem {
+        
+        return items[index].galleryItem
+    }
+}
+
+extension Image3dPrinterViewController: GalleryItemsDelegate {
+    
+    func removeGalleryItem(at index: Int) {
+        
+        print("remove item at \(index)")
+        
+        let imageView = items[index].imageView
+        imageView.removeFromSuperview()
+        items.remove(at: index)
+    }
+}
+
+// Some external custom UIImageView we want to show in the gallery
+class FLSomeAnimatedImage: UIImageView {
+}
+
+// Extend ImageBaseController so we get all the functionality for free
+class AnimatedViewController: ItemBaseController<FLSomeAnimatedImage> {
+}
