c9a656822283c335abc74b936f46cf46421d910d
Dinh Nhinh <dinhnhinh@Dinhs-MacBook.local>
2019-03-05 10:34:50 +0700

    remove 'Delete' and 'Select All' in Print3DVC

--- PLTi2019/ViewControllers/Image3dPrinterViewController.swift
+++ PLTi2019/ViewControllers/Image3dPrinterViewController.swift
@@ -128,50 +128,19 @@ extension Image3dPrinterViewController: UICollectionViewDataSource, UICollection
         
         return [
             
-            GalleryConfigurationItem.closeButtonMode(.builtIn),
+            // Remove two buttons 'Delete' & 'Show all'
+            GalleryConfigurationItem.deleteButtonMode(ButtonMode.none),
+            GalleryConfigurationItem.thumbnailsButtonMode(ButtonMode.none),
             
-            GalleryConfigurationItem.pagingMode(.standard),
-            GalleryConfigurationItem.presentationStyle(.displacement),
-            GalleryConfigurationItem.hideDecorationViewsOnLaunch(false),
+            // Disable bluring background while ImageViewer dismiss
+            GalleryConfigurationItem.overlayBlurOpacity(0),
             
-            GalleryConfigurationItem.swipeToDismissMode(.vertical),
-            GalleryConfigurationItem.toggleDecorationViewsBySingleTap(false),
-            GalleryConfigurationItem.activityViewByLongPress(false),
+            // Image & its background fade out in the same time (0.2s)
+            GalleryConfigurationItem.reverseDisplacementDuration(0.2),
+            GalleryConfigurationItem.colorDismissDuration(0.2),
             
-            GalleryConfigurationItem.overlayColor(UIColor(white: 0.035, alpha: 1)),
-            GalleryConfigurationItem.overlayColorOpacity(1),
-            GalleryConfigurationItem.overlayBlurOpacity(1),
-            GalleryConfigurationItem.overlayBlurStyle(UIBlurEffect.Style.light),
-            
-            GalleryConfigurationItem.videoControlsColor(.white),
-            
-            GalleryConfigurationItem.maximumZoomScale(8),
-            GalleryConfigurationItem.swipeToDismissThresholdVelocity(500),
-            
-            GalleryConfigurationItem.doubleTapToZoomDuration(0.15),
-            
-            GalleryConfigurationItem.blurPresentDuration(0.5),
-            GalleryConfigurationItem.blurPresentDelay(0),
-            GalleryConfigurationItem.colorPresentDuration(0.25),
-            GalleryConfigurationItem.colorPresentDelay(0),
-            
-            GalleryConfigurationItem.blurDismissDuration(0.1),
-            GalleryConfigurationItem.blurDismissDelay(0.4),
-            GalleryConfigurationItem.colorDismissDuration(0.45),
-            GalleryConfigurationItem.colorDismissDelay(0),
-            
-            GalleryConfigurationItem.itemFadeDuration(0.3),
-            GalleryConfigurationItem.decorationViewsFadeDuration(0.15),
-            GalleryConfigurationItem.rotationDuration(0.15),
-            
-            GalleryConfigurationItem.displacementDuration(0.55),
-            GalleryConfigurationItem.reverseDisplacementDuration(0.25),
-            GalleryConfigurationItem.displacementTransitionStyle(.springBounce(0.7)),
-            GalleryConfigurationItem.displacementTimingCurve(.linear),
-            
-            GalleryConfigurationItem.statusBarHidden(true),
-            GalleryConfigurationItem.displacementKeepOriginalInPlace(false),
-            GalleryConfigurationItem.displacementInsetMargin(50)
+            // Disable bounce
+            GalleryConfigurationItem.displacementTransitionStyle(GalleryDisplacementStyle.normal),
         ]
     }
 }
